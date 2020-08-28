190183c5f6c5341e81058bd20aedd88ad499f5ac
unknown <boureimatapily1@gmail.com>
2020-08-28 02:21:27 +0000

    ADD Susu component

---
+++ src/Components/Susu/Susu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Susu extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default Susu
