65bb43ec49dc118f88602943b2f3363a411cc799
unknown <boureimatapily1@gmail.com>
2020-08-31 22:39:16 +0000

    FIX BUG

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import Dashboard from "./Pages/Dashboard";
-import Hospital from "./Pages/Hospital";
 import Navbar from "./Components/Navbar";
 import { ToastContainer } from "react-toastify"; // ToastContainer to acces to toast notify everywhere in the project
 import 'react-toastify/dist/ReactToastify.css';
 import Login from "./Pages/Login";
-import EditPatient from "./Components/Hospital/Patient/EditPatient";
-import PatientList from "./Components/Hospital/Patient/PatientList";
-//import EditDoctor from "./Components/Hospital/Doctor/EditDoctor";
-// import EditSingleDoctor from "./Components/Hospital/Doctor/EditSingleDoctor";
-import AdminPatientTab from "./Components/Hospital/AdminPatientTab";
+
 import EditGroup from "./Components/Susu/Susu_Group/EditGroup";
 import EditSusuType from "./Components/Susu/Susu_frequency/EditSusuType";
 
@@ -24,13 +19,9 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/adminpatienttab" component={AdminPatientTab} />
-        <Route exact path="/hospital" component={Hospital} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/edit/:id" component={EditPatient} />
         <Route exact path="/editgroup/:id" component={EditGroup} />
         <Route exact path="/singlesusu/:id" component={EditSusuType} />
-        <Route exact path="/patientlist" component={PatientList} />
       </Switch>
     </React.Fragment>
   );
