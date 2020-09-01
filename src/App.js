134c5929b320579fde668796427196f6d15dc068
unknown <boureimatapily1@gmail.com>
2020-09-01 11:27:01 +0000

    MOD polish the app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "./Pages/Home";
-import Dashboard from "./Pages/Dashboard";
+//import { Route, Switch } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import { ToastContainer } from "react-toastify"; // ToastContainer to acces to toast notify everywhere in the project
+//import { ToastContainer } from "react-toastify"; // ToastContainer to acces to toast notify everywhere in the project
 import 'react-toastify/dist/ReactToastify.css';
-import Login from "./Pages/Login";
-
-import EditGroup from "./Components/Susu/Susu_Group/EditGroup";
-import EditSusuType from "./Components/Susu/Susu_frequency/EditSusuType";
-
 
 function App() {
   return (
     <React.Fragment>
-      <ToastContainer />
+
+      <h1>hello twitter</h1>
+      {/* <ToastContainer />
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
+      <Switch> */}
+        {/* <Route exact path="/" component={Home} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/editgroup/:id" component={EditGroup} />
         <Route exact path="/singlesusu/:id" component={EditSusuType} />
-      </Switch>
+      </Switch> */}
     </React.Fragment>
   );
 }
