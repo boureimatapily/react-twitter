1e097c8861076da2231b283cb004c9095577c610
unknown <boureimatapily1@gmail.com>
2020-09-01 13:06:44 +0000

    ADD icons

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import SidebarItem from "./SidebarItem"
+import TwitterIcon from '@material-ui/icons/Twitter';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+
 function Sidebar() {
     return (
         <div>
             <div className="sidebar">
-                <SidebarItem />
-                <SidebarItem />
-                <SidebarItem />
-                <SidebarItem />
+                <TwitterIcon/>
+                < SidebarItem Icon={HomeIcon} text="Home" />
+                < SidebarItem Icon={SearchIcon} text="Explore" />
+                < SidebarItem Icon={NotificationsNoneIcon} text="Notifcations" />
+                < SidebarItem Icon={MailOutlineIcon} text="Messages" />
+                < SidebarItem Icon={BookmarkBorderIcon} text="Bookmarks" />
+                < SidebarItem Icon={ListAltIcon} text="Lists" />
+                < SidebarItem Icon={PermIdentityIcon} text="Profile" />
+                < SidebarItem Icon={MoreHorizIcon} text="More" />
+                
+                
             </div>
         </div>
     )
--- src/Components/Sidebar/SidebarItem.js
+++ src/Components/Sidebar/SidebarItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function SidebarItem({props}) {
+function SidebarItem({text, Icon}) {
     return (
         <div>
             <div className="sidebarItem">
-                <h2>item</h2>
+                <Icon />
+                <h2> {text} </h2>
             </div>
         </div>
     )
